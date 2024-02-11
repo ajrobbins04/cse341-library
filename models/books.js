60f095e423dba881b292bf7d124170bca82a4123
ajrobbins04 <ajrobbins04@gmail.com>
2024-02-10 21:56:17 -0800

    Book model added

---
+++ models/books.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// define shape of documents w/in contacts collection
+const bookSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  author: String,
+  numAvailable: Number,
+  numTotal: Number,
+  yearPublished: Number,
+});
+
+// create Book model (model name used by mongoose, not the db)
+const Book = mongoose.model('Book', bookSchema);
+
+// export Book model as module
+module.exports = Book;
