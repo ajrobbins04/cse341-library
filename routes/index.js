46b30d081c2fe72aa8ee5b826e360232a05b49b9
ajrobbins04 <ajrobbins04@gmail.com>
2024-04-07 20:43:14 -0700

    routes error corrected

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { requiresAuth } = require('express-openid-connect');
 const bookRoutes = require('./books');
+const authorRoutes = require('./authors');
 const swaggerRoutes = require('./swagger');
 const indexController = require('../controllers/index');
 const authController = require('../controllers/auth');
@@ -11,5 +12,6 @@ router.use('/', swaggerRoutes); // mount to be included in apiDocs
 router.get('/', authController.authenticate); // handles user auth internally
 router.get('/profile', requiresAuth(), authController.renderUserProfile);
 router.use('/books', bookRoutes);
+router.use('/authors', authorRoutes);
 
 module.exports = router;
